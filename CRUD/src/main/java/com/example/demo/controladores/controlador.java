81a3a66b45ac17402328f1dcc66751d0a1d91899
hvega25 <2023_Herson.Vega@iticbcn.cat>
2023-10-05 16:22:36 +0200

    CRUD

--- CRUD/src/main/java/com/example/demo/controladores/controlador.java
+++ CRUD/src/main/java/com/example/demo/controladores/controlador.java
@@ -5,18 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.bean.Usuario;
+
+import org.springframework.ui.Model;
+
 @Controller
 @RequestMapping("")
 public class controlador {
 
-	
 	@GetMapping("/")
-	public String iniciar() {
-		return "login";		
+	public String iniciar(Model model) {
+		// Importante al importar la libreria import org.springframework.ui.Model;
+		model.addAttribute("titulo", "FORMULARIO DE ACCESO");
+		return "login";
 	}
-	
+
 	@PostMapping("/")
-	public String login(){
-		return "consultar";
+	public String login(Usuario usuario, Model model) {
+		if (usuario.getNombre().equals("edu") && usuario.getPassword().equals("edu")) {
+			model.addAttribute("usuario", usuario);
+			return "consulta";
+		} else
+			return "login";
+
 	}
 }
